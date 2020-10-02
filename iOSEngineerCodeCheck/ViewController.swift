6047dbdb83ae12bda308fcd52a82dbeacb0cd77b
YoshikiTsukada <tsukadayyy@gmail.com>
2020-10-02 11:35:57 +0900

    Apply SwiftFormat

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -9,41 +9,39 @@
 import UIKit
 
 class ViewController: UITableViewController, UISearchBarDelegate {
-
     @IBOutlet weak var SchBr: UISearchBar!
-    
-    var repo: [[String: Any]]=[]
-    
+
+    var repo: [[String: Any]] = []
+
     var task: URLSessionTask?
     var word: String = ""
     var url: String = ""
     var idx: Int?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         SchBr.text = "GitHubのリポジトリを検索できるよー"
         SchBr.delegate = self
     }
-    
+
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         // ↓こうすれば初期のテキストを消せる
         searchBar.text = ""
         return true
     }
-    
+
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         task?.cancel()
     }
-    
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         word = searchBar.text ?? ""
-        
+
         if word.count != 0 {
             url = "https://api.github.com/search/repositories?q=\(word)"
             guard let url = URL(string: url) else { return }
-            task = URLSession.shared.dataTask(with: url) { [weak self] (data, _, err) in
+            task = URLSession.shared.dataTask(with: url) { [weak self] data, _, err in
                 if let err = err {
                     print(err)
                     return
@@ -62,44 +60,37 @@ class ViewController: UITableViewController, UISearchBarDelegate {
                     print(error)
                 }
             }
-        // これ呼ばなきゃリストが更新されません
-        task?.resume()
+            // これ呼ばなきゃリストが更新されません
+            task?.resume()
         }
-        
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "Detail"{
+        if segue.identifier == "Detail" {
             guard let dtl = segue.destination as? ViewController2 else {
                 assertionFailure()
                 return
             }
             dtl.vc1 = self
         }
-        
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repo.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = UITableViewCell()
         let rp = repo[indexPath.row]
         cell.textLabel?.text = rp["full_name"] as? String ?? ""
         cell.detailTextLabel?.text = rp["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
-        
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
         idx = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
-        
     }
-    
 }
--- iOSEngineerCodeCheck/ViewController2.swift
+++ iOSEngineerCodeCheck/ViewController2.swift
@@ -9,23 +9,22 @@
 import UIKit
 
 class ViewController2: UIViewController {
-    
     @IBOutlet weak var ImgView: UIImageView!
-    
+
     @IBOutlet weak var TtlLbl: UILabel!
-    
+
     @IBOutlet weak var LangLbl: UILabel!
-    
+
     @IBOutlet weak var StrsLbl: UILabel!
     @IBOutlet weak var WchsLbl: UILabel!
     @IBOutlet weak var FrksLbl: UILabel!
     @IBOutlet weak var IsssLbl: UILabel!
-    
+
     var vc1: ViewController?
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         guard let idx = vc1?.idx, let repo = vc1?.repo[idx] else { return }
 
         LangLbl.text = "Written in \(repo["language"] as? String ?? "")"
@@ -34,19 +33,17 @@ class ViewController2: UIViewController {
         FrksLbl.text = "\(repo["forks_count"] as? Int ?? 0) forks"
         IsssLbl.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
-        
     }
-    
-    func getImage(){
-        
+
+    func getImage() {
         guard let idx = vc1?.idx, let repo = vc1?.repo[idx] else { return }
-        
+
         TtlLbl.text = repo["full_name"] as? String
-        
+
         if let owner = repo["owner"] as? [String: Any] {
             if let imgURL = owner["avatar_url"] as? String {
                 guard let url = URL(string: imgURL) else { return }
-                URLSession.shared.dataTask(with: url) { (data, _, err) in
+                URLSession.shared.dataTask(with: url) { data, _, err in
                     if let err = err {
                         print(err)
                         return
@@ -60,7 +57,5 @@ class ViewController2: UIViewController {
                 }.resume()
             }
         }
-        
     }
-    
 }
